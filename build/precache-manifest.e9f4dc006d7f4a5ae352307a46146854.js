c5148a5acff48f886baba10abc846ec54fdffd59
Maxence <maxence.califano@gmail.com>
2022-05-26 17:38:20 +0200

    fix: gh page

---
+++ build/precache-manifest.e9f4dc006d7f4a5ae352307a46146854.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d6927d5d4b313d1906dd4a92a0f54d",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "65f544a257f788d417d5",
+    "url": "/React-Calculator/static/css/main.bcb3cf94.chunk.css"
+  },
+  {
+    "revision": "ca1399890265d7e9ea51",
+    "url": "/React-Calculator/static/js/2.5c59175a.chunk.js"
+  },
+  {
+    "revision": "65f544a257f788d417d5",
+    "url": "/React-Calculator/static/js/main.83b43203.chunk.js"
+  },
+  {
+    "revision": "19a8db08324e9b1cabe9",
+    "url": "/React-Calculator/static/js/runtime~main.cb778861.js"
+  }
+]);
